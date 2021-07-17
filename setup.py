6b3f2c5bf86ace624024da37f6d711975d937250
HimaAne <86917309+HimaAne@users.noreply.github.com>
2021-07-17 16:00:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name="robotframework-PyWindowsGuiLibrary",
-    version="1.1",
+    version="2.0",
     author="Himavanthudu Bodapati",
     author_email="himavanthudu.b@gmail.com",
     description="A Robot Framework Library for automating the WINDOWS BASED GUI applications",
